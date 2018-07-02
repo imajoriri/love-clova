828faa48a3a54e1540868a2d7afc1c408af10741
imajoriri <30540418+imajoriri@users.noreply.github.com>
2018-07-02 17:49:59 +0900

    update init.js and create makeIndex.js

--- script/init.js
+++ script/init.js
@@ -9,71 +9,9 @@ function writeFile(path, data) {
   });
 }
 
-var createFileName = "index.js";
-
-// require
-var req = `
-var clova = require("love-clova");
-`
-
-// handler
-var handlersList = ["LaunchRequest", "Clova.GuideIntent", "sampleIntent"]
-var error = "error"
-var handlers = handlersList.map( (intent) => {
-  intent = intent.replace(".", ""); // ビルトインインテントに"."があるので排除
-  return handlerCreater(intent);
-}).join("");
-var exports = exportsCreater(handlersList, error);
-
-const errorHandler = `
-const errorHandler = {
-  canHandle: function(handlerInput){
-    return true;
-  },
-  handle: function(handlerInput){
-    var msg = "エラー発生";
-    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
-  }
-}
-`
-
-var indexFile = req + handlers + errorHandler + exports;
-
+// index file
+const makeIndex = require('./makeIndex.js');
+const createFileName = makeIndex.createFileName;
+const indexFile = makeIndex.indexFile;
 writeFile(createFileName, indexFile);
-
-// log
-console.log(`create ${createFileName} file`);
-                                   
-
-// functions
-function handlerCreater(intent){
-  var text = `
-const ${intent}Handler = {
-  canHandle: function(handlerInput){
-    return handlerInput.requestEnvelope.isMatch(${"'" + intent + "'"});
-  },
-  handle: function(handlerInput){
-    var msg = "";
-    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
-  }
-}
-`
-  return text;
-}
-
-function exportsCreater(intents, error){
-  var RequestHandlers = intents.map( (intent) => {
-    intent = intent.replace(".", ""); // ビルトインインテントに"."があるので排除
-    return `${intent}Handler`;
-  }).join(",");
-  var errorHandler = `${error}Handler`;
-  return `
-exports.handler = clova.extensionBuilders
-  .addRequestHandlers(${RequestHandlers})
-  .addErrorHandlers(${errorHandler})
-  .lambda()
-`
-  return RequestHandlers;
-}
-
 
---
+++ script/makeIndex.js
@@ -0,0 +1,70 @@
+
+
+// require
+var req = `
+var clova = require("love-clova");
+`
+
+// handler
+var handlersList = ["LaunchRequest", "Clova.GuideIntent", "sampleIntent"]
+var error = "error"
+var handlers = handlersList.map( (intent) => {
+  intent = intent.replace(".", ""); // ビルトインインテントに"."があるので排除
+  return handlerCreater(intent);
+}).join("");
+var exports = exportsCreater(handlersList, error);
+
+const errorHandler = `
+const errorHandler = {
+  canHandle: function(handlerInput){
+    return true;
+  },
+  handle: function(handlerInput){
+    var msg = "エラー発生";
+    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
+  }
+}
+`
+
+var indexFile = req + handlers + errorHandler + exports;
+var createFileName = "index.js";
+
+console.log(`create ${createFileName} file`);
+
+// ------exports------
+module.exports.indexFile = indexFile;
+module.exports.createFileName = createFileName;
+
+
+// ------functions------
+function handlerCreater(intent){
+  var text = `
+const ${intent}Handler = {
+  canHandle: function(handlerInput){
+    return handlerInput.requestEnvelope.isMatch(${"'" + intent + "'"});
+  },
+  handle: function(handlerInput){
+    var msg = "";
+    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
+  }
+}
+`
+  return text;
+}
+
+function exportsCreater(intents, error){
+  var RequestHandlers = intents.map( (intent) => {
+    intent = intent.replace(".", ""); // ビルトインインテントに"."があるので排除
+    return `${intent}Handler`;
+  }).join(",");
+  var errorHandler = `${error}Handler`;
+  return `
+exports.handler = clova.extensionBuilders
+  .addRequestHandlers(${RequestHandlers})
+  .addErrorHandlers(${errorHandler})
+  .lambda()
+`
+  return RequestHandlers;
+}
+
+
